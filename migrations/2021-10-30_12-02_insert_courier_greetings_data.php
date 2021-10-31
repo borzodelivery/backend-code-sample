0f7ee3ad0da219bc831e6c3ee16aa97e5b45d0f7
Anton Kotik <cat@borzodelivery.com>
2021-11-01 00:30:55 +0300

    Update code sample

--- migrations/2021-10-30_12-02_insert_courier_greetings_data.php
+++ migrations/2021-10-30_12-02_insert_courier_greetings_data.php
@@ -1,6 +1,5 @@
 <?php
 
-use Dostavista\Core\Super;
 use Dostavista\Framework\Database\Migrations\MysqlMigrationAbstract;
 
 return new class() extends MysqlMigrationAbstract {
@@ -13,10 +12,9 @@ return new class() extends MysqlMigrationAbstract {
 
     protected function execute(): void {
         if (Super::getConfig()->isRussia()) {
-            Super::getDb()->query("
-                INSERT INTO courier_greetings
-                    (greeting_template, allowed_to_show_start_time, allowed_to_show_finish_time)
-                VALUES 
+            $this->getDb()->query("
+                INSERT INTO courier_greetings (greeting_template, allowed_to_show_start_time, allowed_to_show_finish_time)
+                VALUES
                     ('Привет, %name%!', '00:00:00', '23:59:59'),
                     ('С возвращением!', '00:00:00', '23:59:59'),
                     ('Доброй ночи, %name%!', '00:00:00', '04:59:59'),
@@ -25,10 +23,9 @@ return new class() extends MysqlMigrationAbstract {
                     ('Добрый вечер, %name%!', '18:00:00', '23:59:59')
             ");
         } else {
-            Super::getDb()->query("
-                INSERT INTO courier_greetings
-                    (greeting_template, allowed_to_show_start_time, allowed_to_show_finish_time)
-                VALUES 
+            $this->getDb()->query("
+                INSERT INTO courier_greetings (greeting_template, allowed_to_show_start_time, allowed_to_show_finish_time)
+                VALUES
                     ('Hello, %name%!', '00:00:00', '23:59:59'),
                     ('Welcome back!', '00:00:00', '23:59:59'),
                     ('Good night %name%!', '00:00:00', '04:59:59'),
